8897cef1a696f5b5b0ce2eaf910c7ee2bde51dde
Shannon Williams <williamsshannonj@gmail.com>
2023-07-24 23:53:09 -0500

    add data labels and multiline x tick labels placeholders

--- chartjs-discord.js
+++ chartjs-discord.js
@@ -7,10 +7,12 @@ let chartEmbed = {};
 // This function will return MessageAttachment object from discord.js
 // Pass as much parameter as you need
 const generateScoreChart = async (scores) => {
-    let labels = scores.map((s) => { return s["date"] });
+    let labels = scores.map((s) => { return [s["date"].slice(0,-5), '2023'] });
     let data = scores.map((s) => { return s["amount"] });
 
-    const renderer = new ChartJSNodeCanvas({ width: 800, height: 600 });
+    const renderer = new ChartJSNodeCanvas({ width: 800, height: 600, plugins: {
+        modern: ['chartjs-plugin-datalabels']
+    }});
     const image = await renderer.renderToBuffer({
         type: 'bar',
         data: {
@@ -18,14 +20,23 @@ const generateScoreChart = async (scores) => {
             datasets: [
                 {
                     data: data,
-                    backgroundColor: '#026623',
+                    backgroundColor: 'rgba(2, 102, 35, 0.2)',
                     borderColor: '#026623',
-                    borderWidth: 0,
+                    borderWidth: 3,
+                    label: "Score",
+                    datalabels: {
+                        align: 'end',
+                        anchor: 'end',
+                        color: 'black',
+                        font: {
+                            size: 22
+                        }
+                      }
                 },
             ],
         },
         options: {
-            layout: { padding: { bottom: 20, top: 25, left: 10, right: 10 } },
+            layout: { padding: { bottom: 20, top: 50, left: 10, right: 10 } },
             scales: {
                 x: {
                     ticks: { font: { size: 25 }, color: '#fff' },
@@ -43,14 +54,29 @@ const generateScoreChart = async (scores) => {
             },
             plugins: {
                 datalabels: {
-                    anchor: 'center',
-                    align: 'center',
-                    color: '#0b3062',
-                    font: {
-                        weight: 'bold',
+                    backgroundColor: '#cbcbcb',
+                      borderRadius: 10,
+                      font: {
+                        weight: 'normal'
+                      },
+                      formatter: function(value, context) {
+                        return value.toLocaleString('en-US');
+                      }
+                      ,
+                      padding: 7
                     },
-                },
-                legend: { display: false }
+                    legend: { display: false }
+            },
+
+            // Core options
+            aspectRatio: 5 / 3,
+            layout: {
+                padding: {
+                    top: 50,
+                    right: 16,
+                    bottom: 0,
+                    left: 8
+                }
             },
         },
     });
